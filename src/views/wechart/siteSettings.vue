5ed5ad254253c16d4aaf35edb3d433e9a2d82788
torres-sky <zhangchen@rinlink.vip>
2021-03-23 09:32:20 +0800

    系统设置

--- src/views/wechart/siteSettings.vue
+++ src/views/wechart/siteSettings.vue
@@ -101,9 +101,9 @@ export default{
      return {
          pickerOptions: {
             disabledDate(time) {
-                return time.getTime() > Date.now();
-                }
-            },
+                return time.getTime() > Date.now()
+            }
+        },
         height:1000,
         value:null,
         loading:false,
