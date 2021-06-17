256a0bcbb4cea5804b99acd1b72a7b0c073bf03e
torres-sky <zhangchen@rinlink.vip>
2021-06-17 18:20:23 +0800

    客户下级列表展示

--- src/views/wechart/sellManage.vue
+++ src/views/wechart/sellManage.vue
@@ -250,7 +250,8 @@ export default{
         timevalue:null,
         usageYearsoptions:[
           {value: 1,label: '1年'},{value: 2,label: '2年'},{value: 3,label: '3年'},
-          {value: 5,label: '5年'},{value: -1,label: '无限制'}
+          {value: 5,label: '5年'},
+          // {value: -1,label: '无限制'}
         ],
         activationoptions:[
           {value: 0,label: '开机激活'},{value: 1,label: '注册激活'},{value: 2,label: '固定时间激活'}
